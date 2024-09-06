54e233b48242580b3f45f8af80c3f9c5cbc6a456
unknown <strivelinkband@yahoo.com>
2024-09-05 19:38:46 -0500

    Update to Report component

--- front-end/src/components/Reports/CreateReport/CreateReport.js
+++ front-end/src/components/Reports/CreateReport/CreateReport.js
@@ -1,8 +1,19 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./CreateReport.scss";
 
 export default function CreateReport() {
+  const initialFormData = {
+    name: "",
+    category: "",
+    questions: [],
+  };
+
   const [listOfQuestions, setListOfQuestions] = useState([]);
+  const [formState, setFormState] = useState(0);
+  const [formData, setFormData] = useState({ ...initialFormData });
+
+  const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -10,11 +21,37 @@ export default function CreateReport() {
 
   const handleCancel = (event) => {
     event.preventDefault();
+    setListOfQuestions([]);
+    setFormState(0);
+    navigate("/reports");
+  };
+
+  const incrementFormState = (event) => {
+    event.preventDefault();
+    setFormState((previous) => (previous += 1));
+  };
+
+  const decrementFormState = (event) => {
+    event.preventDefault();
+    setFormState((previous) => (previous -= 1));
   };
 
   const addQuestion = (event) => {
     const value = event.target.value;
-    setListOfQuestions([...listOfQuestions, value]);
+    const questionElement = (
+      <div key={value} className="Question">
+        <label htmlFor="question-title" className="form-label">
+          Question title
+        </label>
+        <input type={value} id="question-title" className="form-control" />
+      </div>
+    );
+    setListOfQuestions([...listOfQuestions, questionElement]);
+  };
+
+  const handleFormDataChange = (event) => {
+    const target = event.target;
+    setFormData((data) => ({ ...data, [target.name]: target.value }));
   };
 
   return (
@@ -22,15 +59,31 @@ export default function CreateReport() {
       <div className="CreateReport-container">
         <h1 className="CreateReport-title">Create a Report!</h1>
         <form onSumbit={handleSubmit} className="CreateReport-fields">
-          <div>
-            <label htmlFor="report-name" className="form-label">
-              Report Name
-            </label>
-            <input type="text" id="report-name" className="form-control" />
-          </div>
-          <div>
-            {listOfQuestions.length === 0 ? (
-              <select onChange={addQuestion} className="form-select">
+          {formState === 0 && (
+            <div>
+              <label htmlFor="report-name" className="form-label">
+                Report Name
+              </label>
+              <input
+                type="text"
+                id="report-name"
+                name="name"
+                className="form-control"
+                onChange={handleFormDataChange}
+                value={formData.name}
+              />
+            </div>
+          )}
+          {formState === 1 && (
+            <div>
+              <label htmlFor="Question-select" className="form-label">
+                Select what type of question to add
+              </label>
+              <select
+                id="Question-select"
+                onChange={addQuestion}
+                className="form-select"
+              >
                 <option selected hidden>
                   Add a New Question
                 </option>
@@ -40,25 +93,24 @@ export default function CreateReport() {
                 <option value="phone">Phone</option>
                 <option value="password">Password</option>
               </select>
-            ) : (
-              listOfQuestions.map((question) => (
-                <div className="Question">
-                  <label htmlFor="question-title" className="form-label">
-                    Question title
-                  </label>
-                  <input id="question-title" className="form-control" />
-                  {/* TODO: Figure out how to handle question generation for new questions. 
-                  Make the Create a Question button stay on screen */}
-                </div>
-              ))
-            )}
-          </div>
+              {listOfQuestions.length !== 0 && listOfQuestions}
+            </div>
+          )}
+
           <div className="form-buttons">
-            <button type="submit" className="btn btn-primary">
-              Next
-            </button>
             <button onClick={handleCancel} className="btn btn-secondary">
               Cancel
+            </button>
+            {formState !== 0 && (
+              <button
+                onClick={decrementFormState}
+                className="btn btn-secondary"
+              >
+                Back
+              </button>
+            )}
+            <button onClick={incrementFormState} className="btn btn-primary">
+              Next
             </button>
           </div>
         </form>
